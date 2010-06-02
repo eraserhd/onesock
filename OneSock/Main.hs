e0ba33c7599503b02d174a413782588f1d3dd2dd
Jason Felice <jason.m.felice@gmail.com>
2010-06-02 19:46:55 -0400

    Implement --scan option
    In the spirit of TSTTCPW, for now we are shelling out to scanimage and
    convert (from Imagemagick) before importing the file into the scan
    database.

--- OneSock/Main.hs
+++ OneSock/Main.hs
@@ -3,6 +3,10 @@ module Main where
 
 import Test.HUnit
 import System.Random
+import System.IO
+import System.Exit
+import System.Directory
+import System.Process
 import System.Environment (getArgs)
 import System.Console.GetOpt
 import OneSock.DB.Scans
@@ -15,12 +19,13 @@ tests = TestList [
           testsForOnesockDBScans
         ]
 
-data Mode = Test | StoreScan
+data Mode = Test | DoScan | StoreScan
 data Flag = ModeFlag Mode
 
 usageHeader = "Usage: OneSock [OPTION...]"
 options =
-  [ Option ['s']  ["store-scan"]  (NoArg (ModeFlag StoreScan))  "store scan(s) to scan database"
+  [ Option ['s']  ["scan"]        (NoArg (ModeFlag DoScan))     "scan and store to database"
+  , Option ['S']  ["store-scan"]  (NoArg (ModeFlag StoreScan))  "store scan(s) to scan database"
   , Option []     ["test"]        (NoArg (ModeFlag Test))       "run unit tests"
   ]
 
@@ -35,6 +40,23 @@ findMode fs =
       []           -> error "No mode argument supplied"
       [ModeFlag m] -> m
       _            -> error "Only one mode argument can be supplied"
+
+doScan :: DB -> IO ()
+doScan db = do
+  id <- randomIO :: IO UUID
+  tmpDir <- getTemporaryDirectory
+  let tiffFile = tmpDir ++ "/" ++ show id ++ ".tiff"
+  let pngFile = tmpDir ++ "/" ++ show id ++ ".png"
+  rc <- system $ "scanimage --format=tiff --resolution=300 >" ++ tiffFile
+  when (rc /= ExitSuccess) $ error "scanimage failed (is sane-utils installed?)"
+  rc <- system $ "convert -scale 800 " ++ tiffFile ++ " " ++ pngFile
+  when (rc /= ExitSuccess) $ error "convert failed (is imagemagick installed?)"
+  bitmap <- GD.loadPngFile pngFile >>= fromGD
+  removeFile tiffFile
+  removeFile pngFile
+  let scan = Scan{scanId=id, scanBitmap=bitmap}
+  storeScan db scan
+  putStrLn $ show id
 
 doStoreScan :: DB -> FilePath -> IO ()
 doStoreScan db filename = do
@@ -55,6 +77,10 @@ main = do
     Test -> do
       runTestTT tests
       return ()
+    DoScan -> do
+      db <- getDefaultDBPath >>= initDB
+      doScan db
+      
     StoreScan -> do
       db <- getDefaultDBPath >>= initDB
       case as of
